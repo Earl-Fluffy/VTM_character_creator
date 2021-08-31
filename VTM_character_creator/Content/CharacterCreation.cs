65f440ffd504849e452a838e2c7ac3bce4754c48
Richard Marchant <39084971+Raderblob@users.noreply.github.com>
2021-08-31 14:29:30 +0200

    content_file_reading: start of character creation types and backing up of content

--- VTM_character_creator/Content/CharacterCreation.cs
+++ VTM_character_creator/Content/CharacterCreation.cs
@@ -9,13 +9,16 @@ namespace VTM_character_creator.Content
     class CharacterCreation
     {
         private string name;
+        private string description;
         private (uint, uint)[] attributeDistribution;
         private (uint, uint)[] skillsDistribution;
         private (uint, uint)[] disciplineDistribution;
         private uint advantagePool;
         private uint flawPool;
+        private uint baseHumanity;
+        private uint baseXP;
 
-        public CharacterCreation(string name, (uint, uint)[] attributeDistribution, (uint, uint)[] skillsDistribution, (uint, uint)[] disciplineDistribution, uint advantagePool, uint flawPool)
+        public CharacterCreation(string name, (uint, uint)[] attributeDistribution, (uint, uint)[] skillsDistribution, (uint, uint)[] disciplineDistribution, uint advantagePool, uint flawPool, uint baseHumanity, uint baseXP)
         {
             this.name = name;
             this.attributeDistribution = attributeDistribution;
@@ -23,6 +26,8 @@ namespace VTM_character_creator.Content
             this.disciplineDistribution = disciplineDistribution;
             this.advantagePool = advantagePool;
             this.flawPool = flawPool;
+            this.baseHumanity = baseHumanity;
+            this.baseXP = baseXP;
         }
     }
 }
